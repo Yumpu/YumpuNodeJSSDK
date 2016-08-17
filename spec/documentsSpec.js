27519efd6213505accbbbc36f0d3f3baf9ccc8b1
Lukas Bals <lukibals@gmx.at>
2016-08-17 14:11:01 +0200

    documents tests finished

--- spec/documentsSpec.js
+++ spec/documentsSpec.js
@@ -19,3 +19,28 @@ describe("DocumentsTest", function() {
         done();
     });
 });
+
+describe("DocumentsTest with limit and offset", function() {
+
+    // include yumpu sdk
+    var yumpu = require('../yumpu');
+    var data = {
+        limit: 1,
+        offset: 0
+    };
+    var status;
+
+    beforeEach(function(done) {
+        var y = new yumpu();
+        y.getDocuments(data, function(statusCode, documents) {
+          status = statusCode;
+          done();
+        });
+    });
+
+
+    it("return status schould be 200", function(done) {
+        expect(status).toEqual(200);
+        done();
+    });
+});
