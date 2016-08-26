13c0280def6aa316aaad8763b80886884bba25fd
Lukas Bals <lukibals@gmx.at>
2016-08-26 11:37:47 +0200

    documentation ...

--- example/user.js
+++ example/user.js
@@ -3,8 +3,51 @@ var yumpu = require('../yumpu');
 // make an instance of the Yumpu sdk class;
 var y = new yumpu();
 
-// get a user details
-// more details on : http://developers.yumpu.com/api/user/get/
+/**
+ * @api {get} /user.json getUser()
+ * @apiVersion 1.0.0
+ * @apiName User get
+ * @apiGroup User
+ *
+ * @apiHeaderExample {js} Java-Script-Example:
+ *  var yumpu = require('yumpu.js');
+ *  var parameters = {
+ *     return_fields: 'id,create_date,activate_date,last_login_date,username,email,gender,name,firstname,lastname,birth_date,address,zip_code,city,country,description,website,blog,languge'
+ *  };
+ *  yumpu.getUser(parameters, function(statusCode, document){
+ *     console.log('Status: ' + statusCode);
+ *     console.log(document);
+ *  });
+ *
+ * @apiParam {String} [return_fields] Customize the responses by setting the return fields (id, create_date, activate_date, last_login_date, username, email, gender, name, firstname, lastname, birth_date, address, zip_code, city, country, description, website, blog, language).
+      <br/><br/><b>Default:</b> id, create_date, activate_date, last_login_date, username, email, gender, name, firstname, lastname, birth_date, address, zip_code, city, country, description, website, blog, languge
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+      "user": {
+        "id": "102864144",
+        "create_date": "2013-03-08 17:13:43",
+        "activate_date": "2013-04-25 14:20:04",
+        "username": "Api.User",
+        "email": "yumpu.api@gmail.com",
+        "gender": "male",
+        "name": "Api User",
+        "firstname": "Api",
+        "lastname": "User",
+        "birth_date": "1986-01-01",
+        "address": "Moosackerstr. 17",
+        "zip_code": "9444",
+        "city": "Diepoldsau",
+        "country": "CH",
+        "description": "I am the API user.",
+        "website": "http://www.yumpu.com",
+        "blog": "http://blog.yumpu.com",
+        "language": "de"
+      },
+      "state": "success"
+    }
+ */
 var parameters = '';
 y.getUser(parameters, function(statusCode, document) {
     console.log('Status: ' + statusCode)
