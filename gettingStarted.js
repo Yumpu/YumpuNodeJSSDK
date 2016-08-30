e2c564017b7d02b6a3fa0d4fb378e5e55623daff
Lukas Bals <lukibals@gmx.at>
2016-08-30 15:54:56 +0200

    ...

--- gettingStarted.js
+++ gettingStarted.js
@@ -10,19 +10,19 @@
  * @apiHeaderExample {java} Install in java:
  * $ java install yumpu
  *
- * @apiSuccessExample {js} Set token in node:
- * @apiSuccessExample {java} Set token in java: 
- *
- * @apiErrorExample {js} How to use in node:
+ * @apiSuccessExample {js} How to use in node:
  *    // initialize the yumpu class
  *    var yumpu = require('yumpu');
+ *
+ *    // set your token (find your token: https://www.yumpu.com/account/profile/api)
+ *    yumpu.setToken('yourToken');
  *
  *    // example to get all your documents
  *    yumpu.getDocuments(parameters, function(statusCode, document) {
  *      console.log('STATUS: ' + statusCode);
  *      console.log(document);
  *    });
- * @apiErrorExample {java} How to use in java:
+ * @apiSuccessExample {java} How to use in java:
  *    var yumpu = require('yumpu');
  *    yumpu.getDocuments(parameters, function(statusCode, document) {
  *      console.log('STATUS: ' + statusCode);
